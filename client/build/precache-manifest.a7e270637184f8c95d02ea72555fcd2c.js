75b96180ed0c1ec2733fa1bdd403e5650d8da8a2
dthompson999 <63125447+dthompson999@users.noreply.github.com>
2020-07-24 13:17:59 -0500

    added build

---
+++ client/build/precache-manifest.a7e270637184f8c95d02ea72555fcd2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc872fbe04d273095e2fb11a5ca7850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d057c0e8a3929e02a2d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "065f22c05a7aa51a78be",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d057c0e8a3929e02a2d",
+    "url": "/static/js/2.a21390be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a21390be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065f22c05a7aa51a78be",
+    "url": "/static/js/main.d8eaa462.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
